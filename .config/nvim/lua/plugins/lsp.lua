f6f03f9cbcf1dbef5f80aa9df8ab5ad59f32a29b
Fibalious <noahlykins@gmail.com>
2025-02-23 02:23:07 -0800

    updated async semantic highlighting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,6 +21,59 @@ local function grab_server_names()
   return mason_servers
 end
 
+local function setup_lsp_keybindings()
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code action" })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Goto definition" })
+  vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, { desc = "LSP: Goto references" })
+  vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "LSP: Goto implementation" })
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "LSP: Type definition" })
+  vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "LSP: Document symbols" })
+  vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "LSP: Workspace symbols" })
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover documentation" })
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature help" })
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: Goto declaration" })
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "LSP: Add workspace folder" })
+  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "LSP: Remove workspace folder" })
+  vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, { desc = "LSP: List workspace folders" })
+  vim.api.nvim_buf_create_user_command(0, "Format", function() vim.lsp.buf.format() end, { desc = "LSP: Format current buffer" })
+end
+
+local function setup_lsp_servers()
+  local lspconfig = require("lspconfig")
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+  local function on_attach(client, bufnr)
+  end
+
+  for _, server in ipairs(servers) do
+    lspconfig[server[1]].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = server[2].settings,
+    }
+  end
+
+  lspconfig.lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        runtime = { version = "LuaJIT", path = { "lua/?.lua", "lua/?/init.lua" } },
+        diagnostics = { globals = { "vim" } },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+        telemetry = { enable = false },
+      },
+    },
+  }
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "sh",
+    callback = function()
+      vim.lsp.start({ name = "bash-language-server", cmd = { "bash-language-server", "start" } })
+    end,
+  })
+end
 
 return {
   { "preservim/tagbar", lazy = false },
@@ -68,54 +121,8 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code action" })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Goto definition" })
-      vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, { desc = "LSP: Goto references" })
-      vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "LSP: Goto implementation" })
-      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "LSP: Type definition" })
-      vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "LSP: Document symbols" })
-      vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "LSP: Workspace symbols" })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover documentation" })
-      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature help" })
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: Goto declaration" })
-      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "LSP: Add workspace folder" })
-      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "LSP: Remove workspace folder" })
-      vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, { desc = "LSP: List workspace folders" })
-      vim.api.nvim_buf_create_user_command(0, "Format", function() vim.lsp.buf.format() end, { desc = "LSP: Format current buffer" })
-
-      local function on_attach(client, bufnr)
-        print("LSP attached: " .. client.name)
-      end
-
-      for _, server in ipairs(servers) do
-        lspconfig[server[1]].setup {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        }
-      end
-
-      lspconfig.lua_ls.setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT", path = { "lua/?.lua", "lua/?/init.lua" } },
-            diagnostics = { globals = { "vim" } },
-            workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
-            telemetry = { enable = false },
-          },
-        },
-      }
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "sh",
-        callback = function()
-          vim.lsp.start({ name = "bash-language-server", cmd = { "bash-language-server", "start" } })
-        end,
-      })
+      setup_lsp_keybindings()
+      setup_lsp_servers()
     end,
   },
 }
