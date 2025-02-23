3c6f61a97d34006b2c7e2e4c423f08d91f07f395
Fibalious <noahlykins@gmail.com>
2025-02-22 17:28:59 -0800

    edited nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,24 +1,35 @@
-
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#6b7273" })
 vim.api.nvim_set_hl(0, "Comment", { fg = "#FF0000" })
 vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
 vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
 
-vim.o.statusline = "%f" 
+vim.o.statusline = "%f"
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "catppuccin",
+  callback = function()
+    -- Apply transparency
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
 
+    local async_color = "#FF69B4" -- Hot pink 
+    local await_color = "#FF1493" -- Deep pink 
+
+    vim.api.nvim_set_hl(0, "@function.async", { fg = async_color })
+    vim.api.nvim_set_hl(0, "@call.async", { fg = async_color })
+    vim.api.nvim_set_hl(0, "@keyword.coroutine", { fg = await_color })
+  end,
+})
 
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
-  opt = {
+  priority = 999999,
+  opts = {
     transparent_background = true,
   },
-  setup = function()
-    vim.cmd("colorscheme catppuccin-mocha") 
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
   end,
 }
