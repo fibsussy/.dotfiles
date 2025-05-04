b81496e1328e2841b28849c9ef5012bb5e556ab9
Fibalious <noahlykins@gmail.com>
2025-05-04 02:15:10 -0700

    updated gists to have links and create new

---
+++ .config/nvim/lua/plugins/gist.lua
@@ -0,0 +1,40 @@
+return {
+
+  {
+    {
+      "linrongbin16/gitlinker.nvim",
+      cmd = "GitLink",
+      opts = {},
+      keys = {
+        { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+        { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+      },
+    },
+  },
+
+
+  {
+    "Rawnly/gist.nvim",
+    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+    opts = {
+      private = true, 
+      clipboard = "+",
+      split_direction = "vertical",
+      gh_cmd = "gh",
+      list = {
+        mappings = {
+          next_file = "<C-n>",
+          prev_file = "<C-p>"
+        },
+      },
+    },
+  },
+  -- `GistsList` opens the selected gist in a terminal buffer,
+  -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+  -- and prevents neovim buffer inception
+  {
+    "samjwill/nvim-unception",
+    lazy = false,
+    init = function() vim.g.unception_block_while_host_edits = true end
+  },
+}
