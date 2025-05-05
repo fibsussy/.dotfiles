cbd5452bb78059877ff71f250fa23366f18a2b3d
Fibalious <noahlykins@gmail.com>
2025-05-05 14:55:27 -0700

    fixed formatting in conflict.lua

--- .config/nvim/lua/plugins/conflict.lua
+++ .config/nvim/lua/plugins/conflict.lua
@@ -1,15 +1,17 @@
 return {
-    'akinsho/git-conflict.nvim',
-    event = "BufRead",
-    version = "*",
-    config = {
-        default_mappings = true,
-        default_commands = true,
-        disable_diagnostics = false,
-        list_opener = 'copen',
-        highlights = {
-            incoming = 'DiffAdd',
-            current = 'DiffText',
-        }
-    }
+    {
+        "akinsho/git-conflict.nvim",
+        event = "BufRead",
+        version = "*",
+        config = {
+            default_mappings = true,
+            default_commands = true,
+            disable_diagnostics = false,
+            list_opener = 'copen',
+            highlights = {
+                incoming = 'DiffAdd',
+                current = 'DiffText',
+            },
+        },
+    },
 }
