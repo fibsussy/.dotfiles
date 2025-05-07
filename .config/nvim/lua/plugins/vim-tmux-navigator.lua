4dc40a7537927311ea8ce3e4ec215d3a0c042e02
fibsussy <noahlykins@gmail.com>
2025-05-07 03:40:34 -0700

    made both C-arrows and M-arrows move windows

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,6 +9,10 @@ return {
       "NavigatorPrevious",
     },
     keys = {
+      { "<M-Left>", "<cmd>NavigatorLeft<cr>", desc = "Navigator: Window Left" },
+      { "<M-Down>", "<cmd>NavigatorDown<cr>", desc = "Navigator: Window Down" },
+      { "<M-Up>", "<cmd>NavigatorUp<cr>", desc = "Navigator: Window Up" },
+      { "<M-Right>", "<cmd>NavigatorRight<cr>", desc = "Navigator: Window Right" },
       { "<C-Left>", "<cmd>NavigatorLeft<cr>", desc = "Navigator: Window Left" },
       { "<C-Down>", "<cmd>NavigatorDown<cr>", desc = "Navigator: Window Down" },
       { "<C-Up>", "<cmd>NavigatorUp<cr>", desc = "Navigator: Window Up" },
