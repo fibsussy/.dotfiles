a5d3e10b57fd1b10d151003465233fedf8aac970
Fibalious <noahlykins@gmail.com>
2025-05-13 09:04:08 -0700

    moved plugin into dependencies

--- .config/nvim/lua/plugins/gist.lua
+++ .config/nvim/lua/plugins/gist.lua
@@ -15,6 +15,16 @@ return {
 
   {
     "Rawnly/gist.nvim",
+    dependencies = {
+      -- `GistsList` opens the selected gist in a terminal buffer,
+      -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+      -- and prevents neovim buffer inception
+      {
+        "samjwill/nvim-unception",
+        lazy = false,
+        init = function() vim.g.unception_block_while_host_edits = true end
+      },
+    }
     cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
     opts = {
       private = true, 
@@ -28,13 +38,5 @@ return {
         },
       },
     },
-  },
-  -- `GistsList` opens the selected gist in a terminal buffer,
-  -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
-  -- and prevents neovim buffer inception
-  {
-    "samjwill/nvim-unception",
-    lazy = false,
-    init = function() vim.g.unception_block_while_host_edits = true end
   },
 }
