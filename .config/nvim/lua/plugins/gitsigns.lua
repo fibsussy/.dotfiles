447470add5a383023e072c6cce7c869404cb2bbf
Fibalious <noahlykins@gmail.com>
2025-05-14 18:54:55 -0700

    fixed gitsigns to lazy load right

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -11,33 +11,30 @@ return {
         changedelete = { text = '~' },
       },
       current_line_blame = false,
+      numhl = true,
     },
-    config = function()
-      local gs = require('gitsigns')
-
-      vim.keymap.set("n", "]c", function()
+    keys = {
+      { "]c", function()
         if vim.wo.diff then return "]c" end
-        vim.schedule(function() gs.nav_hunk("next") end)
+        vim.schedule(function() require('gitsigns').nav_hunk("next") end)
         return "<Ignore>"
-      end, { desc = "Gitsigns next hunk", expr = true, silent = true, noremap = true })
-
-      vim.keymap.set("n", "[c", function()
+      end, desc = "Gitsigns next hunk", expr = true, silent = true, noremap = true },
+      { "[c", function()
         if vim.wo.diff then return "[c" end
-        vim.schedule(function() gs.nav_hunk("prev") end)
+        vim.schedule(function() require('gitsigns').nav_hunk("prev") end)
         return "<Ignore>"
-      end, { desc = "Gitsigns previous hunk", expr = true, silent = true, noremap = true })
-
-      vim.keymap.set({ "n", "v" }, "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Gitsigns stage hunk", silent = true, noremap = true })
-      vim.keymap.set({ "n", "v" }, "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Gitsigns reset hunk", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { desc = "Gitsigns stage buffer", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>ha", gs.stage_hunk, { desc = "Gitsigns stage hunk", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hR", gs.reset_buffer, { desc = "Gitsigns reset buffer", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { desc = "Gitsigns preview hunk", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hb", function() gs.blame_line { full = true } end, { desc = "Gitsigns blame line", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "Gitsigns toggle blame", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hd", gs.diffthis, { desc = "Gitsigns diff this", silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>hD", function() gs.diffthis('~') end, { desc = "Gitsigns diff HEAD", silent = true, noremap = true })
-      vim.keymap.set({ "o", "x" }, "ih", "<cmd>Gitsigns select_hunk<CR>", { desc = "Gitsigns select hunk", silent = true, noremap = true })
-    end,
+      end, desc = "Gitsigns previous hunk", expr = true, silent = true, noremap = true },
+      { "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", mode = { "n", "v" }, desc = "Gitsigns stage hunk", silent = true, noremap = true },
+      { "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", mode = { "n", "v" }, desc = "Gitsigns reset hunk", silent = true, noremap = true },
+      { "<leader>hS", function() require('gitsigns').stage_buffer() end, desc = "Gitsigns stage buffer", silent = true, noremap = true },
+      { "<leader>ha", function() require('gitsigns').stage_hunk() end, desc = "Gitsigns stage hunk", silent = true, noremap = true },
+      { "<leader>hR", function() require('gitsigns').reset_buffer() end, desc = "Gitsigns reset buffer", silent = true, noremap = true },
+      { "<leader>hp", function() require('gitsigns').preview_hunk() end, desc = "Gitsigns preview hunk", silent = true, noremap = true },
+      { "<leader>hb", function() require('gitsigns').blame_line { full = true } end, desc = "Gitsigns blame line", silent = true, noremap = true },
+      { "<leader>tB", function() require('gitsigns').toggle_current_line_blame() end, desc = "Gitsigns toggle blame", silent = true, noremap = true },
+      { "<leader>hd", function() require('gitsigns').diffthis() end, desc = "Gitsigns diff this", silent = true, noremap = true },
+      { "<leader>hD", function() require('gitsigns').diffthis('~') end, desc = "Gitsigns diff HEAD", silent = true, noremap = true },
+      { "ih", "<cmd>Gitsigns select_hunk<CR>", mode = { "o", "x" }, desc = "Gitsigns select hunk", silent = true, noremap = true },
+    },
   }
 }
