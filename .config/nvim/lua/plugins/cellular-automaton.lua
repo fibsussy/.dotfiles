f3a2e226b19e70317fdb0717964626ba7bb4cbbf
Fibalious <noahlykins@gmail.com>
2025-05-17 00:18:45 -0700

    upd

--- .config/nvim/lua/plugins/cellular-automaton.lua
+++ .config/nvim/lua/plugins/cellular-automaton.lua
@@ -1,12 +1,23 @@
 return {
   "Eandrju/cellular-automaton.nvim",
-  lazy = true,
   cmd = { "CellularAutomaton" },
+  keys = {
+    {
+      "<leader>rb",
+      function()
+        local animations = {}
+        local ca = require("cellular-automaton")
+        for name, _ in pairs(ca.animations) do
+          table.insert(animations, name)
+        end
+        ca.start_animation(animations[math.random(#animations)])
+      end,
+      desc = "CellularAutomaton random animation",
+    },
+  },
   config = function()
-    local ca = require("cellular-automaton")
-    
-    ca.register_animation {
-      fps = 80,
+    require("cellular-automaton").register_animation {
+      fps = 144,
       name = "slide",
       update = function(grid)
         for i = 1, #grid do
@@ -18,13 +29,5 @@ return {
         return true
       end,
     }
-
-    vim.keymap.set("n", "<leader>rb", function()
-      local animations = {}
-      for name, _ in pairs(ca.animations) do
-        table.insert(animations, name)
-      end
-      ca.start_animation(animations[math.random(#animations)])
-    end, { desc = "CellularAutomaton random animation" })
   end,
 }
