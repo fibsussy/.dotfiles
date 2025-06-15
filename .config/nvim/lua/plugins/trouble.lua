806a1dd0ef29b5b9c63639fa98e179e2e1c8ab23
Fibalious <noahlykins@gmail.com>
2025-06-15 15:36:38 -0700

    fixed trouble

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,7 +1,8 @@
 return {
   {
     "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
     keys = {
       {
         "<leader>xx",
@@ -36,6 +37,3 @@ return {
     },
   },
 }
-
-
-
