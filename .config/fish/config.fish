d1bb208cf557da3a37fa53c9254fce4df7894e09
fibsussy <noahlykins@gmail.com>
2025-07-15 01:19:52 -0700

    fixing fish to organize

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,38 @@
 if not status is-interactive
- exit
+    exit
 end
 
-
-set -x XDG_CONFIG_HOME "$HOME/.config"
-set -x XDG_CACHE_HOME "$HOME/.cache"
-set -x XDG_DATA_HOME "$HOME/.local/share"
-set -x XDG_STATE_HOME "$HOME/.local/state"
-set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
-set -x RUSTFLAGS '-W clippy::pedantic -W clippy::nursery -A clippy::unreadable_literal -A clippy::struct_excessive_bools'
-set -x PYENV_ROOT "$HOME/.pyenv"
-set -x PYENV_VIRTUALENV_DISABLE_PROMPT 1
-set -x LANG en_US.UTF-8
-set -x EDITOR nvim
-set -x LESSHISTFILE "$XDG_STATE_HOME/less/history"
-set -x PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
-set -x WGETRC "$XDG_CONFIG_HOME/wgetrc"
-set -x SCREENRC "$XDG_CONFIG_HOME/screen/screenrc"
-set -x MANPAGER 'nvim -c "Man!" -c "set buftype=nofile modifiable"'
-
+set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+set -gx RUSTFLAGS '-W clippy::pedantic -W clippy::nursery -A clippy::unreadable_literal -A clippy::struct_excessive_bools'
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -gx PYENV_VIRTUALENV_DISABLE_PROMPT 1
+set -gx LANG en_US.UTF-8
+set -gx EDITOR nvim
+set -gx LESSHISTFILE "$XDG_STATE_HOME/less/history"
+set -gx PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
+set -gx WGETRC "$XDG_CONFIG_HOME/wgetrc"
+set -gx SCREENRC "$XDG_CONFIG_HOME/screen/screenrc"
+set -gx MANPAGER 'nvim -c "Man!" -c "set buftype=nofile modifiable"'
+set -Ux SSH_AUTH_SOCK /run/user/(id -u)/ssh-agent.socket
 
 fish_add_path -g $HOME/.cargo/bin $PYENV_ROOT/bin $HOME/.local/bin /usr/local/bin /usr/local/sbin
 
-set -g fish_greeting ""
+#tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=Yes
+set -U tide_left_prompt_suffix ""
+set -U tide_left_prompt_items character
+set -U tide_right_prompt_items status vi_mode pwd cmd_duration context git jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+
+set -U tide_vi_mode_icon_insert ""
+set -U tide_vi_mode_icon_default "Normal"
+set -U tide_vi_mode_icon_replace "Replace"
+set -U tide_vi_mode_icon_visual "Visual"
+
+set -U fish_greeting ""
 set -g fish_history_size 50000
-set -g fish_history_file "$XDG_STATE_HOME/fish/history"
+
 fish_vi_key_bindings
 bind \ce edit_command_buffer
-
-set -g fish_complete_path $HOME/.zfunc $fish_complete_path
+bind \cs sudo_toggle
 
 function load_environment_files
     if test -f ~/.api_keys.env
@@ -44,48 +48,6 @@ function load_environment_files
         source .env.development
     end
 end
-load_environment_files
-
-
-
-abbr -a rm 'rm -i'
-abbr -a tm 'trash'
-abbr -a cd 'z'
-abbr -a .. 'cd ..'
-abbr -a ... 'cd ../..'
-abbr -a .3 'cd ../../..'
-abbr -a .4 'cd ../../../..'
-abbr -a .5 'cd ../../../../..'
-abbr -a mkdir 'mkdir -pv'
-abbr -a l 'eza -lh --icons=auto'
-abbr -a ls 'eza -1 --icons=auto'
-abbr -a ll 'eza -lha --icons=auto --sort=name --group-directories-first'
-abbr -a ld 'eza -lhD --icons=auto'
-abbr -a lt 'eza --icons=auto --tree'
-abbr -a cat 'bat'
-abbr -a v 'nvim'
-abbr -a sc 'systemctl'
-abbr -a scu 'systemctl --user'
-abbr -a grep 'grep --color=auto'
-abbr -a fgrep 'grep -F --color=auto'
-abbr -a egrep 'grep -E --color=auto'
-abbr -a diff 'diff --color=auto'
-abbr -a ip 'ip --color=auto'
-
-abbr -a --position anywhere H -- "--help"
-abbr -a --position anywhere L -- "| $MANPAGER"
-abbr -a --position anywhere G -- '| grep'
-abbr -a --position anywhere W -- '| wc'
-abbr -a --position anywhere J -- '| jq'
-abbr -a --position anywhere C -- '| tee /dev/tty | perl -pe "chomp if eof" | wl-copy'
-abbr -a --position anywhere CC -- '| tee /dev/tty | begin; echo "❯ $ZSH_COMMAND"; perl -pe "chomp if eof"; end | wl-copy'
-abbr -a --position anywhere null '/dev/null'
-abbr -a --position anywhere 2null '&>/dev/null'
-abbr -a --position anywhere 2bg '&>/dev/null & disown'
-
-alias brb 'clear; figlet BRB | lolcat'
-alias fastfetch '~/.config/fastfetch/fastfetch_inline.sh'
-
 
 function tmux_force --description 'Force tmux session'
     if not command -v tmux >/dev/null
@@ -124,12 +86,6 @@ function sudo_toggle
         commandline "sudo $cmd"
     end
 end
-bind \cs sudo_toggle
-
-
-abbr -a sudo 'sudo '
-abbr -a touch 'mkdir -p (dirname $argv[1]) && command touch $argv[1]'
-abbr -a paruclean 'sudo pacman -Rsn (pacman -Qdtq 2>/dev/null)'
 
 function paru
     if command -v paru >/dev/null
@@ -171,3 +127,47 @@ function stow_dotfiles
         return 1
     end
 end
+
+# Aliases
+alias brb 'clear; figlet BRB | lolcat'
+alias fastfetch '~/.config/fastfetch/fastfetch_inline.sh'
+
+# Abbreviations
+abbr -a rm 'rm -i'
+abbr -a tm 'trash'
+abbr -a cd 'z'
+abbr -a .. 'cd ..'
+abbr -a ... 'cd ../..'
+abbr -a .3 'cd ../../..'
+abbr -a .4 'cd ../../../..'
+abbr -a .5 'cd ../../../../..'
+abbr -a mkdir 'mkdir -pv'
+abbr -a l 'eza -lh --icons=auto'
+abbr -a ls 'eza -1 --icons=auto'
+abbr -a ll 'eza -lha --icons=auto --sort=name --group-directories-first'
+abbr -a ld 'eza -lhD --icons=auto'
+abbr -a lt 'eza --icons=auto --tree'
+abbr -a cat 'bat'
+abbr -a v 'nvim'
+abbr -a sc 'systemctl'
+abbr -a scu 'systemctl --user'
+abbr -a grep 'grep --color=auto'
+abbr -a fgrep 'grep -F --color=auto'
+abbr -a egrep 'grep -E --color=auto'
+abbr -a diff 'diff --color=auto'
+abbr -a ip 'ip --color=auto'
+abbr -a touch 'mkdir -p (dirname $argv[1]) && command touch $argv[1]'
+abbr -a paruclean 'sudo pacman -Rsn (pacman -Qdtq 2>/dev/null)'
+
+abbr -a --position anywhere H -- "--help"
+abbr -a --position anywhere L -- "| $MANPAGER"
+abbr -a --position anywhere G -- '| grep'
+abbr -a --position anywhere W -- '| wc'
+abbr -a --position anywhere J -- '| jq'
+abbr -a --position anywhere C -- '| tee /dev/tty | perl -pe "chomp if eof" | wl-copy'
+abbr -a --position anywhere CC -- '| tee /dev/tty | begin; echo "❯ $ZSH_COMMAND"; perl -pe "chomp if eof"; end | wl-copy'
+abbr -a --position anywhere null '/dev/null'
+abbr -a --position anywhere 2null '&>/dev/null'
+abbr -a --position anywhere 2bg '&>/dev/null & disown'
+
+zoxide init fish | source
