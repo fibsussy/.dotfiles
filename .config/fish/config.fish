c00163aac3a80739a4fe51dcb8fee335b940cf2a
fibsussy <noahlykins@gmail.com>
2025-07-20 05:06:03 -0700

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,18 +17,6 @@ set -Ux SSH_AUTH_SOCK /run/user/(id -u)/ssh-agent.socket
 
 fish_add_path -g $HOME/.cargo/bin $PYENV_ROOT/bin $HOME/.local/bin /usr/local/bin /usr/local/sbin
 
-# tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=Yes
-set -g tide_left_prompt_items pwd character
-set -g tide_right_prompt_items status vi_mode pwd cmd_duration context git jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
-set -g tide_left_prompt_suffix
-set -g tide_vi_mode_icon_insert
-set -g tide_vi_mode_icon_default Normal
-set -g tide_vi_mode_icon_replace Replace
-set -g tide_vi_mode_icon_visual Visual
-
-set -g fish_greeting ""
-set -g fish_history_size 50000
-
 fish_vi_key_bindings
 set -g fish_cursor_default block
 set -g fish_cursor_insert block
@@ -37,25 +25,45 @@ set -g fish_cursor_replace underscore
 set -g fish_cursor_external block
 set -g fish_cursor_visual block
 
+# tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=Yes
+set -gx tide_left_prompt_items character
+set -gx tide_right_prompt_items status vi_mode pwd cmd_duration context git jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+set -gx tide_left_prompt_suffix ""
+set -gx tide_vi_mode_icon_insert ""
+set -gx tide_vi_mode_icon_default "Normal"
+set -gx tide_vi_mode_icon_replace "Replace"
+set -gx tide_vi_mode_icon_visual "Visual"
+
+set -g fish_greeting ""
+set -g fish_history_size 50000
+
 bind \ce edit_command_buffer
 bind \cs sudo_toggle
 
 function load_environment_files
-    if test -f ~/.api_keys.env
-        source ~/.api_keys.env
-    end
-    if test -f .env
-        source .env
-    end
-    if test -f .env.dev
-        source .env.dev
-    end
-    if test -f .env.development
-        source .env.development
+    for file in ~/.api_keys.env .env .env.dev .env.development
+        if test -f $file
+            while read -l line
+                # Skip empty lines and full-line comments
+                if string match -qr '^\s*#' $line || string match -qr '^\s*$' $line
+                    continue
+                end
+                # Remove inline comments and parse KEY=VALUE
+                set -l clean_line (string replace -r '\s*#.*$' '' $line | string trim)
+                if test -n "$clean_line"
+                    set -l key (string split -m 1 '=' $clean_line | head -n 1 | string trim)
+                    set -l value (string split -m 1 '=' $clean_line | tail -n 1 | string trim)
+                    set -gx $key $value
+                end
+            end < $file
+        end
     end
 end
-
+load_environment_files
+ 
 
 source ~/.config/fish/abbreviations.fish
-
+if test -f .python-version
+    pyenv init - fish | source
+end
 zoxide init fish | source
