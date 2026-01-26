2d47e5c040731557099634744222aa65964453d6
fibsussy <noahlykins@gmail.com>
2026-01-26 04:04:23 -0800

    fixed navigation to other sessions in nvim

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,59 +1,3 @@
--- Setup our custom navigation immediately (not lazy-loaded)
-vim.keymap.set('n', '<M-Left>', function()
-  -- Try to move left in nvim first
-  local original_win = vim.fn.winnr()
-  vim.cmd('wincmd h')
-  local current_win = vim.fn.winnr()
-  
-  -- If we didn't move (same window), try tmux previous window
-  if current_win == original_win then
-    local cur_tmux_win = tonumber(vim.fn.system('tmux display -p "#{window_index}" 2>/dev/null'):gsub('%s+', '') or '1')
-    if cur_tmux_win > 1 then
-      vim.fn.system('tmux previous-window')
-    end
-  end
-end, { desc = 'Smart Navigate Left', silent = true })
-
-vim.keymap.set('n', '<M-Right>', function()
-  -- Try to move right in nvim first
-  local original_win = vim.fn.winnr()
-  vim.cmd('wincmd l')
-  local current_win = vim.fn.winnr()
-  
-  -- If we didn't move (same window), try tmux next window
-  if current_win == original_win then
-    local cur_tmux_win = tonumber(vim.fn.system('tmux display -p "#{window_index}" 2>/dev/null'):gsub('%s+', '') or '1')
-    local total_tmux_wins = tonumber(vim.fn.system('tmux display -p "#{session_windows}" 2>/dev/null'):gsub('%s+', '') or '1')
-    if cur_tmux_win < total_tmux_wins then
-      vim.fn.system('tmux next-window')
-    end
-  end
-end, { desc = 'Smart Navigate Right', silent = true })
-
-vim.keymap.set('n', '<M-Up>', function()
-  -- Try to move up in nvim first
-  local original_win = vim.fn.winnr()
-  vim.cmd('wincmd k')
-  local current_win = vim.fn.winnr()
-  
-  -- If we didn't move (same window), try tmux session up (lower index)
-  if current_win == original_win then
-    vim.fn.system('~/.config/tmux/session_info.sh prev')
-  end
-end, { desc = 'Smart Navigate Up', silent = true })
-
-vim.keymap.set('n', '<M-Down>', function()
-  -- Try to move down in nvim first
-  local original_win = vim.fn.winnr()
-  vim.cmd('wincmd j')
-  local current_win = vim.fn.winnr()
-  
-  -- If we didn't move (same window), try tmux session down (higher index)
-  if current_win == original_win then
-    vim.fn.system('~/.config/tmux/session_info.sh down')
-  end
-end, { desc = 'Smart Navigate Down', silent = true })
-
 return {
   {
     "numToStr/Navigator.nvim",
@@ -65,6 +9,10 @@ return {
       "NavigatorRight",
     },
     keys = {
+      { "<M-Left>", desc = "Smart Navigate Left" },
+      { "<M-Right>", desc = "Smart Navigate Right" },
+      { "<M-Up>", desc = "Smart Navigate Up" },
+      { "<M-Down>", desc = "Smart Navigate Down" },
       { "<C-Left>", "<cmd>NavigatorLeft<cr>", desc = "Navigator: Window Left" },
       { "<C-Down>", "<cmd>NavigatorDown<cr>", desc = "Navigator: Window Down" },
       { "<C-Up>", "<cmd>NavigatorUp<cr>", desc = "Navigator: Window Up" },
@@ -78,6 +26,62 @@ return {
     },
     config = function(_, opts)
       require('Navigator').setup(opts)
+      
+      -- Setup our custom navigation after plugin loads
+      vim.keymap.set('n', '<M-Left>', function()
+        -- Try to move left in nvim first
+        local original_win = vim.fn.winnr()
+        vim.cmd('wincmd h')
+        local current_win = vim.fn.winnr()
+        
+        -- If we didn't move (same window), try tmux previous window
+        if current_win == original_win then
+          local cur_tmux_win = tonumber(vim.fn.system('tmux display -p "#{window_index}" 2>/dev/null'):gsub('%s+', '') or '1')
+          if cur_tmux_win > 1 then
+            vim.fn.system('tmux previous-window')
+          end
+        end
+      end, { desc = 'Smart Navigate Left', silent = true })
+
+      vim.keymap.set('n', '<M-Right>', function()
+        -- Try to move right in nvim first
+        local original_win = vim.fn.winnr()
+        vim.cmd('wincmd l')
+        local current_win = vim.fn.winnr()
+        
+        -- If we didn't move (same window), try tmux next window
+        if current_win == original_win then
+          local cur_tmux_win = tonumber(vim.fn.system('tmux display -p "#{window_index}" 2>/dev/null'):gsub('%s+', '') or '1')
+          local total_tmux_wins = tonumber(vim.fn.system('tmux display -p "#{session_windows}" 2>/dev/null'):gsub('%s+', '') or '1')
+          if cur_tmux_win < total_tmux_wins then
+            vim.fn.system('tmux next-window')
+          end
+        end
+      end, { desc = 'Smart Navigate Right', silent = true })
+
+      vim.keymap.set('n', '<M-Up>', function()
+        -- Try to move up in nvim first
+        local original_win = vim.fn.winnr()
+        vim.cmd('wincmd k')
+        local current_win = vim.fn.winnr()
+        
+        -- If we didn't move (same window), try tmux session up
+        if current_win == original_win then
+          vim.fn.system('~/.config/tmux/scripts/tmux_sessions.sh prev')
+        end
+      end, { desc = 'Smart Navigate Up', silent = true })
+
+      vim.keymap.set('n', '<M-Down>', function()
+        -- Try to move down in nvim first
+        local original_win = vim.fn.winnr()
+        vim.cmd('wincmd j')
+        local current_win = vim.fn.winnr()
+        
+        -- If we didn't move (same window), try tmux session down
+        if current_win == original_win then
+          vim.fn.system('~/.config/tmux/scripts/tmux_sessions.sh down')
+        end
+      end, { desc = 'Smart Navigate Down', silent = true })
     end,
   },
 }
