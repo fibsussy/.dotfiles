2563e10ce407887cfd745e9198bf03c8866971b9
fibsussy <noahlykins@gmail.com>
2026-02-05 23:18:40 -0800

    upd

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -21,7 +21,7 @@ return {
     -- Help
     { "<leader>?", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
     -- Top Pickers & Explorer
-    { "<leader>F", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    -- { "<leader>F", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
@@ -32,7 +32,7 @@ return {
     { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
     { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    -- { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     -- git
     -- { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
