4623de756a92b6a8351d689e52ca8fe8f7ab521a
noahlyk <noahlykins@gmail.com>
2026-09-09 18:23:12 -0600

    upd

---
+++ .config/mozilla/firefox/kvsdi6lh.default-release/user.js
@@ -0,0 +1,15 @@
+// Material Fox (DMS live theming)
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("userChrome.theme-material", true);
+// Speed / Reliability (Arch Wiki Firefox)
+user_pref("layout.frame_rate", 320);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("network.dnsCacheEntries", 1000);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("browser.cache.disk.capacity", 1048576);
+user_pref("browser.sessionstore.interval", 60000);
+user_pref("browser.sessionstore.max_tabs_undo", 10);
+user_pref("browser.tabs.unloadOnLowMemory", true);
+user_pref("browser.quitShortcut.disabled", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
